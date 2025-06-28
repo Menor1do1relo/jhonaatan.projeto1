2a7c995a8b8f31dd5415230dcf12afb6e8529928
Menor1do1relo <jhonatan.zuber@escola.pr.gov.br>
2025-06-27 22:21:13 -0300

    ç

---
+++ perguntas.js
@@ -0,0 +1,11 @@
+riaCartao (
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao (
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
